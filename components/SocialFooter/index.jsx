b1025c6a92300ed4670fdaf26d596d4c78a63004
Helen <helen@joincolab.io>
2022-06-12 23:30:08 -0700

    Update index.jsx

--- components/SocialFooter/index.jsx
+++ components/SocialFooter/index.jsx
@@ -7,20 +7,20 @@ function SocialFooter() {
       <p className="footer-text inter-normal-white-12px">
         Product Manager Mock Interview Prep
         <br />
-        Built with ♥ ♥ ♥ by Co.Lab
+        Built with ❤️ by Co.Lab
       </p>
       <div className="logo-white-container">        
         <a href="https://joincolab.io" target="_blank">
           <img className="github-logo-white" src="/img/Circle Logo.png" />
         </a>
         <a href="https://www.figma.com/@anima" target="_blank">
-          <img className="ma-logo-white" src="/img/figma-logo-white-1@2x.svg" />
+          <img className="ma-logo-white" src="/img/icons8-twitter-50.png" />
         </a>
-        <a href="https://medium.com/@avishic/how-to-nail-your-yc-interview-e3a4d12871f3" target="_blank">
-          <img className="medium-logo-white" src="/img/medium-logo-white-1@2x.svg" />
+        <a href="https://linkedin.com/company/joincolab" target="_blank">
+          <img className="medium-logo-white" src="/img/icons8-linkedin-2-50.png" />
         </a>
-        <a href="https://animaapp.com/?utm_source=anima-github-live&utm_campaign=yc-prep&utm_medium=anima-github-live" target="_blank">
-          <img className="ma-logo-white" src="/img/anima-logo-white-2@2x.svg" />
+        <a href="https://www.youtube.com/channel/UC3XmSQsq-WfrwWTNf0Ch2kQ" target="_blank">
+          <img className="ma-logo-white" src="/img/icons8-youtube-50.png" />
         </a>
       </div>
     </div>
